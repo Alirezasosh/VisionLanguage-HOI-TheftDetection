8165022e686ff7e20956bc3de911419a15fbed2d
Alirezasosh <Alirezasoltanshahi2000@gmail.com>
2026-08-12 16:28:25 +0330

    Update research pipeline

---
+++ src/evaluation/__init__.py
@@ -0,0 +1,11 @@
+"""
+Evaluation utilities for the VisionLanguage-HOI-TheftDetection project.
+"""
+
+from .metrics import calculate_metrics
+
+__all__ = [
+    "calculate_metrics",
+]
+
+__version__ = "0.1.0"
---
+++ src/models/__init__.py
@@ -0,0 +1,11 @@
+"""
+Model components for the VisionLanguage-HOI-TheftDetection project.
+"""
+
+from .classifier import TheftClassifier
+
+__all__ = [
+    "TheftClassifier",
+]
+
+__version__ = "0.1.0"
