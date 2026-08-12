275a2d06b5f8d5695f64be8841127e0718fa1db4
Alirezasosh <alirezasoltanshahi2000@gmail.com>
2026-08-12 15:47:30 +0330

    Stage 59: expose dataset API

---
+++ src/dataset/__init__.py
@@ -0,0 +1,3 @@
+from .video_dataset import TheftVideoDataset, VideoRecord, discover_videos, records_from_csv, read_video_clip
+
+__all__ = ["TheftVideoDataset", "VideoRecord", "discover_videos", "records_from_csv", "read_video_clip"]
